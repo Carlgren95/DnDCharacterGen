91baa8716f0d3b4d734cb571053902746b697691
Anton Carlgren <69874305+Carlgren95@users.noreply.github.com>
2020-09-20 17:47:59 +0200

    Add files via upload

--- DND 5e Character Generator/Character Generator.cs
+++ DND 5e Character Generator/Character Generator.cs
@@ -16,7 +16,7 @@ namespace DND_5e_Character_Generator
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainForm());
 		}
 	}
 }
--- DND 5e Character Generator/Character.cs
+++ DND 5e Character Generator/Character.cs
@@ -19,8 +19,10 @@ namespace DND_5e_Character_Generator
 		public static string characterSubClass = "";
 		public static string alignment = "";
 
-		public static int characterLevel = 0;
+		public static int proficiency = 2;
+		public static int level = 1;
 		public static int[] abilityScores = new int[] { 0, 0, 0, 0, 0, 0 };
+		public static int[] abilityModifiers = new int[] { 0, 0, 0, 0, 0, 0}; 
 		public static int[] abilityBonuses = new int[] { 0, 0, 0, 0, 0, 0 };
 		public static int armorClass = 0;
 		public static int arnorClassBonus = 0;
@@ -32,10 +34,10 @@ namespace DND_5e_Character_Generator
 
 		public static int[] saves = new int[] { 0, 0, 0, 0, 0, 0 };
 		public static bool[] saveProficiencies = new bool[] { false, false, false, false, false, false };
+
 		public static int[] skills = new int[] { 0, 0, 0, 0, 0, 0,
 												 0, 0, 0, 0, 0, 0,
 												 0, 0, 0, 0, 0, 0, };
-
 		public static bool[] skillProficiencies = new bool[] { false, false, false, false, false, false,
 															   false, false, false, false, false, false,
 															   false, false, false, false, false, false };
